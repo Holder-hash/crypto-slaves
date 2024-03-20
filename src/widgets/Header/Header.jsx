056dff791b284a4e110e524979b1085c5f98f676
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-03-20 21:10:05 +0300

    nav aside

--- src/widgets/Header/Header.jsx
+++ src/widgets/Header/Header.jsx
@@ -1,14 +1,24 @@
 import styles from "./Header.module.scss";
+import { useState } from "react";
 
 export default function Header() {
+  const [navActive, setNavActive] = useState(false);
   return (
-    <header>
-      <button className={styles.navBtn}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-      <h1 className={styles.logo}>CRYPTO SLAVES</h1>
-    </header>
+    <>
+      <nav
+        className={`${styles.nav} ${navActive ? styles.navActive : ""}`}
+      ></nav>
+      <header>
+        <button
+          className={`${styles.navBtn} ${navActive ? styles.navBtnActive : ""}`}
+          onClick={() => setNavActive(!navActive)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <h1 className={styles.logo}>CRYPTO SLAVES</h1>
+      </header>
+    </>
   );
 }
