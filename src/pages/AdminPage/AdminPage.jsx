0adea4fb48991627cde75a19d7850270c3324443
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-03-31 08:43:45 +0300

    admin nav line params

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,6 +1,11 @@
 import styles from "./AdminPage.module.scss";
+import { useState } from "react";
 
 function AdminPage() {
+  const [lineParams, setLineParams] = useState({
+    width: 165,
+    posX: 0,
+  });
   return (
     <div className={styles.wrapper}>
       <nav className={styles.nav}>
@@ -8,6 +13,13 @@ function AdminPage() {
           <li>Все игроки</li>
           <li>Пополнения</li>
           <li>Запросы на вывод</li>
+          <div
+            className={styles.navLine}
+            style={{
+              left: `${lineParams.posX}px`,
+              width: `${lineParams.width}px`,
+            }}
+          ></div>
         </ul>
       </nav>
     </div>
