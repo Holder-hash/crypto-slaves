37bcf5faecb070d2ff5e4eef3ee3eb4c39650214
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-03-31 08:50:49 +0300

    admin nav line move

--- src/pages/AdminPage/AdminPage.jsx
+++ src/pages/AdminPage/AdminPage.jsx
@@ -1,22 +1,37 @@
 import styles from "./AdminPage.module.scss";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function AdminPage() {
   const [lineParams, setLineParams] = useState({
     width: 165,
     posX: 0,
   });
+
+  const navLineRef = useRef(null); // Создаем ссылку на элемент navLine
+
+  function navItemHandler(event) {
+    const navItem = event.target; // Получаем элемент <li>, на который был совершен клик
+    const { left, width } = navItem.getBoundingClientRect(); // Получаем позицию и ширину элемента <li>
+    setLineParams({
+      width: width,
+      posX: left,
+    });
+  }
+
+  console.log(lineParams);
+
   return (
     <div className={styles.wrapper}>
       <nav className={styles.nav}>
         <ul>
-          <li>Все игроки</li>
-          <li>Пополнения</li>
-          <li>Запросы на вывод</li>
+          <li onClick={navItemHandler}>Все игроки</li>
+          <li onClick={navItemHandler}>Пополнения</li>
+          <li onClick={navItemHandler}>Запросы на вывод</li>
           <div
+            ref={navLineRef}
             className={styles.navLine}
             style={{
-              left: `${lineParams.posX}px`,
+              left: `${lineParams.posX - 48}px`,
               width: `${lineParams.width}px`,
             }}
           ></div>
